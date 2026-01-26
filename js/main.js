26fed2c90ed547283200b77a0872e31f0b442eba
z 1 r h <arsen.strilets@student.tdsb.on.ca>
2026-01-25 20:20:37 -0500

    Add const variables for HTML elements to prepare UI updates

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const cityInput = document.getElementBYId("cityInput");
+const searchBtn = document.getElementById("seacrhBtn");
+
+const cityNameElem = document.getELementBYId("cityName");
+const tempElem = document.getElementById("temp");
+const descriptionElem = document.getElementById("description");
+const humidityElem = document.getElementById("humidity");
+const windElem = document.getElementById("wind");

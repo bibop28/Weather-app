f6a39e066221cc02997010bc8ae5c96fd077391c
z 1 r h <arsen.strilets@student.tdsb.on.ca>
2026-01-25 20:45:31 -0500

    Add weatherResult hide/show and fix UI updates

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-const cityInput = document.getElementBYId("cityInput");
-const searchBtn = document.getElementById("seacrhBtn");
+const cityInput = document.getElementById("cityInput");
+const searchBtn = document.getElementById("searchBtn");
 
-const cityNameElem = document.getELementBYId("cityName");
+const cityNameElem = document.getElementById("cityName");
 const tempElem = document.getElementById("temp");
 const descriptionElem = document.getElementById("description");
 const humidityElem = document.getElementById("humidity");
 const windElem = document.getElementById("wind");
+
+
+import { getWeather } from "./api.js";
+import { updateWeatherUI } from "./ui.js";
+
+searchBtn.addEventListener("click", async () => {
+    const city = cityInput.value.trim();
+    if (city === "") {
+        alert("Please enter a city");
+        return;
+    }
+
+    const weatherData = await getWeather(city);
+    updateWeatherUI(weatherData);
+
+});
--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,17 @@
-function updateWeatherUI(data) {
+const cityNameElem = document.getElementById("cityName");
+const tempElem = document.getElementById("temp");
+const descriptionElem = document.getElementById("description");
+const humidityElem = document.getElementById("humidity");
+const windElem = document.getElementById("wind");
+
+export function updateWeatherUI(data) {
     if (!data || data.cod !== 200) {
         alert("City not found!")
         return;
     }
+
+    const weatherResult = document.getElementById("weatherResult");
+    weatherResult.style.display = "block";
 
 
 cityNameElem.textContent = data.name;

5eeec08c3f8e7bc4ede27a7784ea40d802e35182
z 1 r h <arsen.strilets@student.tdsb.on.ca>
2026-02-24 21:50:13 -0500

    ui js

--- js/ui.js
+++ js/ui.js
@@ -6,17 +6,22 @@ const windElem = document.getElementById("wind");
 
 export function updateWeatherUI(data) {
     const weatherResult = document.getElementById("weatherResult");
+    const container = document.querySelector(".app-container");
+
 
     if (!data || data.cod !== 200) {
+        weatherResult.classList.remove("show");
+        container.classList.remove("active");
         alert("City not found!")
         return;
     }
 
     weatherResult.classList.add("show");
+    container.classList.add("active");
 
 
 cityNameElem.textContent = data.name;
-tempElem.textContent = `Temperature: ${data.main.temp}°C`;
+tempElem.textContent = `Temperature: ${Math.round(data.main.temp)}°C`;
 humidityElem.textContent = `Humidity: ${data.main.humidity}%`;
 windElem.textContent = `Wind speed: ${data.wind.speed} m/s`;
 
